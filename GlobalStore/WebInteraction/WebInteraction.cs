3b92d6d6cd416be742417c2e61d6239260f4eaea
alexei andrusceac <alexeiandrusceac@hotmail.com>
2020-01-10 09:06:11 +0200

    vineri

--- GlobalStore/WebInteraction/WebInteraction.cs
+++ GlobalStore/WebInteraction/WebInteraction.cs
@@ -1,11 +1,8 @@
 ﻿using GlobalStore.Entity;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 namespace GlobalStore
 {
